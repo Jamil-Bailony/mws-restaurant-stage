b045f3d9f1178c59e3610bafa09e7cb8fd93a6f4
Jamil Bailony <jameel-b1994@hotmail.com>
2019-04-26 00:22:55 +0300

    feat: adding the accessibility functionality

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' restaurant';
   li.append(image);
 
   const name = document.createElement('h1');
@@ -178,6 +179,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = '2';
+  more.setAttribute('role', 'button');
   li.append(more)
 
   return li
