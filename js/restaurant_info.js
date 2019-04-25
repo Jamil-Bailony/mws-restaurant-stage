2a5a5386f45c27ea893192aa0212c655f23981c4
Jamil Bailony <jameel-b1994@hotmail.com>
2019-04-25 21:44:47 +0300

    feat: adding responsive feature

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamJhaWxvbnkiLCJhIjoiY2p1d2N3cXd1MGE0dDQ0cXIwM2hya2c3ZSJ9.8wWhEQjDgfCe_0HTNvjKDA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
